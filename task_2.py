7e0b0da7aaf5655ba21032504be44d046baadefd
AideNaDa <152689555+AideNaDa@users.noreply.github.com>
2026-02-06 20:35:14 +0100

    PEP8 fix

--- task_2.py
+++ task_2.py
@@ -1,20 +1,24 @@
 import random
 
-def get_numbers_ticket(min: int, max: int, quantity: int) -> list[int]:
+
+def get_numbers_ticket(min_value: int, max_value: int, quantity: int) -> list[int]:
     """
     Generate a sorted list of unique random numbers in the given range.
-    
-    :param min: Minimum value of the range (must be >= 1)
-    :param max: Maximum value of the range (must be <= 1000)
+
+    :param min_value: Minimum value of the range (must be >= 1)
+    :param max_value: Maximum value of the range (must be <= 1000)
     :param quantity: Number of unique random values to generate
     :return: Sorted list of unique random integers or an empty list if input is invalid
     """
-    if not (1 <= min <= max <= 1000) or not (0 < quantity <= (max - min + 1)):
+    if not (1 <= min_value <= max_value <= 1000):
         return []
-    
+
+    if not (0 < quantity <= (max_value - min_value + 1)):
+        return []
+
     try:
-        numbers = random.sample(range(min, max + 1), quantity)
+        numbers = random.sample(range(min_value, max_value + 1), quantity)
     except ValueError:
         return []
-    
+
     return sorted(numbers)
