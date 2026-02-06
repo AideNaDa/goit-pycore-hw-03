e69e82caabe16207be5bfa8417d9d73a06102463
AideNaDa <152689555+AideNaDa@users.noreply.github.com>
2026-02-06 20:38:18 +0100

    PEP8 fix

--- task_1.py
+++ task_1.py
@@ -1,17 +1,17 @@
 from datetime import datetime
 
-def get_days_from_today(data: str) -> int:
+
+def get_days_from_today(data_str: str) -> int:
     """
     Calculates the difference in days between a specified date and the current date
-    
+
     :param data: Days string in 'YYYY-MM-DD' format
     :return: Difference in days
     """
     try:
-        user_data = datetime.strptime(data, '%Y-%m-%d').date()
+        user_data = datetime.strptime(data_str, '%Y-%m-%d').date()
         now_date = datetime.now().date() 
         return (now_date - user_data).days 
-
     except ValueError:
         raise ValueError(
             f"Invalid date format: {data}, expected YYYY-MM-DD"
